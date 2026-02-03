6d1bad602b0de8bde14d6f4e0d669a1af9a8761b
Caylor <caylor777@gmail.com>
2026-02-03 08:32:29 -0500

    Refactor NavBar component to use process.env.PUBLIC_URL for image paths and add target attributes for external links

--- react-app/src/components/navBar.jsx
+++ react-app/src/components/navBar.jsx
@@ -1,12 +1,14 @@
 function NavBar() {
     return (
-    <div id="navBar">
-        <img id="logo" src="/images/logo.png" alt="Auto-NavLog Logo"/>
-        <div id="githubDiv">
-            <a id="githubLink" href="https://github.com/Caylor777">My GitHub</a>
-            <img id="githubLogo" src="/images/github.svg" alt="GitHub Logo"/>
+        <div id="navBar">
+            <img id="logo" src={process.env.PUBLIC_URL + '/images/logo.png'} alt="Auto-NavLog Logo" />
+            <div id="githubDiv">
+                <a id="githubLink" href="https://github.com/Caylor777" target="_blank" rel="noopener noreferrer">
+                    My GitHub
+                    <img id="githubLogo" src={process.env.PUBLIC_URL + '/images/github.svg'} alt="GitHub Logo" />
+                </a>
+            </div>
         </div>
-    </div>
     );
 }
 
